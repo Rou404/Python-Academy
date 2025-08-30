63bc53229dc222d2e668c1c8bd863b1ab03b3896
Casian <57826342+CasianJors@users.noreply.github.com>
2025-08-30 10:31:48 +0300

    M2 L1 P1

---
+++ M2 - Cisco Essentials 2/lesson1 Modules, Packages, pip/part1/example.py
@@ -0,0 +1,26 @@
+# import random as rnd
+#
+# print(rnd.choice('abcd'))
+# print(rnd.choice([1, 2, 3, 4, 5]))
+# print(rnd.randint(0,100))
+#
+#
+#
+#
+# import random as r
+#
+# print(r.choice('abcd'))
+#
+# print('Name of this script is: , __name__)
+#
+# from modul4.seminar1.app1 import check_parola
+#
+# password = "G!@#dsdsdsd4343"
+# check_parola(password)
+#
+# from modul4.seminar2 import example
+# from modul4.seminar2 import app1
+#
+# print(example.my_set1)
+# print('Calcul varsta medie: ', app1.medie([10,22,15]))
+# print('Media este: ', app1.suma([1, 2]))
