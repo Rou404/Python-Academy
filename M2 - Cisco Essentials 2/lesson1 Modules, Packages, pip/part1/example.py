b8cf7269aa0a999c9a3ca0e9816ee67d61055df8
Casian <57826342+CasianJors@users.noreply.github.com>
2025-08-30 11:10:13 +0300

    update

--- M2 - Cisco Essentials 2/lesson1 Modules, Packages, pip/part1/example.py
+++ M2 - Cisco Essentials 2/lesson1 Modules, Packages, pip/part1/example.py
@@ -1,26 +1,19 @@
-# import random as rnd
-#
-# print(rnd.choice('abcd'))
-# print(rnd.choice([1, 2, 3, 4, 5]))
-# print(rnd.randint(0,100))
-#
-#
-#
-#
+import random as rnd
+
+print(rnd.choice('abcd'))
+print(rnd.choice([1, 2, 3, 4, 5]))
+print(rnd.randint(0,100))
+
 # import random as r
 #
 # print(r.choice('abcd'))
+# print(f'Name of this script is: , {__name__}')
 #
-# print('Name of this script is: , __name__)
+# import pachet
 #
-# from modul4.seminar1.app1 import check_parola
 #
-# password = "G!@#dsdsdsd4343"
-# check_parola(password)
+# print(pachet.pi)
 #
-# from modul4.seminar2 import example
-# from modul4.seminar2 import app1
+# import pachet as lectie_noua
 #
-# print(example.my_set1)
-# print('Calcul varsta medie: ', app1.medie([10,22,15]))
-# print('Media este: ', app1.suma([1, 2]))
+# print(lectie_noua.suma(5,10))
---
+++ M2 - Cisco Essentials 2/lesson1 Modules, Packages, pip/part1/pachet.py
@@ -0,0 +1,16 @@
+def suma(a, b):
+    return a+b
+
+pi = 3,14
+nume = "Casi"
+print(__name__)
+
+
+
+
+if __name__ == '__main__':
+    print(1)
+    print(f'ma cheama {nume} si am {suma(20,3)} ani')
+
+if __name__ == 'pachet':
+    print(f'Name of this script is: , {__name__}')
