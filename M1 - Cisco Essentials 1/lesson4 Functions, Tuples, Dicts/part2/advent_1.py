1d8f9d04567d5d4c9a37e84be0c08e5d36421f0f
Stegeran Darius <40896007+Rou404@users.noreply.github.com>
2025-12-03 18:53:38 +0200

    Add files via upload

--- M1 - Cisco Essentials 1/lesson4 Functions, Tuples, Dicts/part2/advent_1.py
+++ M1 - Cisco Essentials 1/lesson4 Functions, Tuples, Dicts/part2/advent_1.py
@@ -1,10 +1,31 @@
 # Mergand la stanga valoare va scadea, respectiv la dreapta valoarea va creste
 
+counter = 0
+pozitie = 50
+
 actiuni = []
 
-f = open("test.txt", "r")
+"""
+R45 -> 45 -> inlocuim "R" cu ""
+L1999 -> -1999 -> inlocuim "L" cu - 
 
-for line in f:
-    actiuni.append(line.strip())
+transformam in int
+"""
+""
+f = open("input.txt", "r")
 
+for linie in f:
+    linie_fara_end = linie.strip()
+    linie_fara_R = linie_fara_end.replace("R", "")
+    linie_cu_L_minus = linie_fara_R.replace("L", "-")
+
+    actiuni.append(int(linie_cu_L_minus))
 print(actiuni)
+
+for actiune in actiuni:
+    pozitie = (pozitie + actiune) % 100
+
+    if pozitie == 0:
+        counter = counter + 1
+
+print(f"Raspunsul este: {counter}")
